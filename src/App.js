5ff20fe11ef12078ad9ae78278e4408399863fde
Houssine <ekhay1@outlook.com>
2024-12-30 19:23:11 -0500

    added the a state object using useReducer, and declaired initialState and the reducer function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
 
+const initialState = {
+  questions: [],
+  // can be loaiding, error, ready, active, finished
+  status: "loading",
+};
+function reducer(state, reducer) {}
 export default function App() {
+  // creating state using reducer
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
-    fetch("http://localhost:8000/questions");
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error fetching data"));
   }, []);
 
   return (
